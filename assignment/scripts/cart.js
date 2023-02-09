4e126a61053d864ba7fb7da54d4ac3cc7e3499e4
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-09 10:25:02 -0600

    completed through listItems required

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,28 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item){
+basket.push (item);
+console.log('Items added to the basket:',item);
+return true;
+}
+console.log('in function addItem. I have added tomato and so this should return true.', addItem('tomato'));
+console.log(`Basket is now ${basket}`);
+console.log('in function addItem. I have added lettuce and so this should return true.', addItem('lettuce'));
+console.log(`Basket is now ${basket}`);
+console.log('in function addItem. I have added bacon and so this should return true.', addItem('bacon'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(){
+    console.log('Here are the items in my basket:');
+for (product of basket){
+    console.log(product);
+}
+}
+listItems()
+
+function empty(){
+
+}
